30c613a2ea6e2dde795c5cae7b91448506666609
Ethan Wipond <ethan.wipond@bambora.com>
2017-02-09 11:13:30 -0800

    Add extra property info to swagger  template

--- source/javascripts/page/swagger.js
+++ source/javascripts/page/swagger.js
@@ -3,15 +3,26 @@ $('.operation-heading').click(function(){
     $(this).next('.operation-content').slideToggle('fast');
 });
 
-// Hide second level schema definitions
+// Hide elements on page load 
 $('.schema-body .schema-body').toggle();
-$('.dots').toggle();
-$('.schema-body .dots').toggle();
+$('.property-body').toggle();
+$('.enum-list').toggle();
 
 // on click collapse object schema definitions
 $('.schema-title').click(function(event) {
     event.preventDefault();
     link_id = $(this).attr('id');
     $(this).siblings('.schema-body').toggle();
-    $(this).siblings('.dots').toggle();
+});
+
+// on click show property details 
+$('.property-type').click(function(event) {
+    event.preventDefault();
+    $(this).siblings('.property-body').toggle();
+})
+
+// on click show enum list 
+$('.enum-link').click(function(event) {
+    event.preventDefault();
+    $(this).siblings('.enum-list').toggle();
 });
