f66d980a1b1706ddcb09bdc69bac1145bdedac4b
Ethan Wipond <ethan.wipond@bambora.com>
2017-02-23 11:40:13 -0800

    Append toc to nav

--- source/javascripts/app/navigation.js
+++ source/javascripts/app/navigation.js
@@ -1,3 +1,5 @@
+/* Javascript for header and side toc */
+
 // Hamburger button/side menu replace header links on 
 // small screens. 
 $(document).ready(function () { 
@@ -22,4 +24,21 @@ $(document).ready(function () {
         event.stopPropagation();
         $('.nav-left').toggleClass('mobile-state-hidden');
     });
+
+    // Combine toc and header side nav if page width below $header-break-width.
+    var $window = $(window);
+    var headerBreakWidth = 875;
+    var combined = false;
+    function checkWidth() {
+        var windowsize = $window.width();
+        if (windowsize < 875) {
+
+            $tableOfContents = $('nav.nav-left');
+            $headerSideNav = $('.header-contents span');
+            $headerSideNav.append($tableOfContents);
+        }
+    }
+    checkWidth();
+    $(window).resize(checkWidth);
+
 });
