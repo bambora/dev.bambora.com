13fe8e1fba4cdb616c4cb1598b924b04740dc6bf
Ethan Wipond <ethan.wipond@bambora.com>
2017-02-22 17:02:35 -0800

    Add expandable side toc

--- source/javascripts/app/navigation.js
+++ source/javascripts/app/navigation.js
@@ -2,9 +2,18 @@
 // small screens. 
 $(document).ready(function () { 
     // Hamburger menu on mobile screen
+    $(".header-contents").toggleClass("mobile-state-hidden")    
     $('.hamburger-icon').click(function(event) {
         event.preventDefault();
         event.stopPropagation();
         $(".header-contents").toggleClass("mobile-state-hidden")
+    });
+
+    // toc button between on mobile screen
+    $('.nav-left').toggleClass('mobile-state-hidden');
+    $('.show-toc').click(function(event) {
+        event.preventDefault();
+        event.stopPropagation();
+        $('.nav-left').toggleClass('mobile-state-hidden');
     });
 });
